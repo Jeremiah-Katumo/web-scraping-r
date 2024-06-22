d16913e7c337755efd082edd3b38396bef375a86
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-06-22 23:57:34 +0300

    clean up

--- 02-cleaning-up.R
+++ 02-cleaning-up.R
@@ -1,4 +1,5 @@
 library(rvest)
+library(tidyverse)
 
 page <- read_html ("https://en.wikipedia.org/wiki/Web_scraping")
 
@@ -13,3 +14,11 @@ page %>%
   strsplit(split = "\n") %>%
   unlist() %>%
   .[. != ""]
+
+body %>% 
+  str_replace_all (pattern = "\n", replacement = " ") %>% 
+  str_replace_all (pattern = "[\\^]", replacement = " ") %>% 
+  str_replace_all (pattern = "\"", replacement = " ") %>% 
+  str_replace_all (pattern = "\\s+", replacement = " ") %>% 
+  str_trim (side = "both") %>% 
+  substr (start = nchar (body_text)-700, stop = nchar (body_text))
